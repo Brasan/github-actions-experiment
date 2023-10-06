3d32b99d7b7c1c82ea420dd9c0460370ec4af32b
Nils Bark <nils.bark@etteplan.com>
2023-10-06 08:56:28 +0200

    Exception in actions test

---
+++ CrashMe/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CrashMe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new NullReferenceException("i have craashed :(");
+        }
+    }
+}
